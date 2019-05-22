22f2315db83b46b77caeb4b0541eb220581fdd4b
jeffityjefferson <jeff.l.kala@gmail.com>
2019-05-21 20:53:27 -0500

    import sys

--- core/peloton_calls.py
+++ core/peloton_calls.py
@@ -11,7 +11,7 @@ def get_credentials(creds_dict=None):
        the input() instead.
     """
     try:
-        with open('~/creds.key') as f:
+        with open('$HOME/creds.key') as f:
             return yaml.safe_load(f)
     except Exception as e:
         pass
